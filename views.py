031b565059fde00fb83d0898b69030fb2b4f19f0
BrightonBTC <carlos@rebelweb.co.uk>
2024-05-12 20:19:28 +0100

    refactor lfg view

--- views.py
+++ views.py
@@ -16,26 +16,7 @@ def welcome():
     sleep(.5)
     center_string('by Kailash', 3)
     sleep(2)
-    
 
-def lfg():
-    lcd.clear()
-    animate_title('LET\'S', 1)
-    sleep(.5)
-    lcd.clear()
-    animate_title('F**!NG', 2)
-    sleep(.5)
-    lcd.clear()
-    animate_title('>>> GO <<<', 3)
-    sleep(.3)
-    lcd.clear()
-    animate_title('>>> GO <<<', 3)
-    sleep(.1)
-    center_string('LET\'S', 1)
-    center_string('F**!NG', 2)
-    center_string('>>> GO <<<', 3)
-    sleep(1)
-    lcd.clear()
 
 class Section:
 
@@ -224,3 +205,72 @@ class SatRatesSection(Section):
         else:
             write_big("{:,}".format(int(self.data["usd"])), "satsusd")
         write_big("{:,}".format(int(self.data["gbp"])), "satsgbp")
+
+
+def lfg():
+    lcd.clear()
+    animate_title('LET\'S', 1)
+    sleep(.5)
+    lcd.clear()
+    animate_title('F**#!NG', 2)
+    sleep(.5)
+    lcd.clear()
+    animate_title('>>> GO <<<', 3)
+    sleep(.3)
+    lcd.clear()
+    animate_title('>>> GO <<<', 3)
+    sleep(.1)
+    center_string('LET\'S', 1)
+    center_string('F**#!NG', 2)
+    center_string('>>> GO <<<', 3)
+    sleep(1)
+    for x in range(3):
+
+        lcd.cursor_pos = (2, 6)
+        lcd.write_string('#')
+        lcd.cursor_pos = (2, 14)
+        lcd.write_string('#')
+        lcd.cursor_pos = (1, 4)
+        lcd.write_string('#')
+        lcd.cursor_pos = (1, 16)
+        lcd.write_string('#')
+        lcd.cursor_pos = (0, 8)
+        lcd.write_string('#')
+        lcd.cursor_pos = (0, 12)
+        lcd.write_string('#')
+
+        sleep(0.01)
+
+        lcd.cursor_pos = (2, 5)
+        lcd.write_string('# ')
+        lcd.cursor_pos = (2, 14)
+        lcd.write_string(' #')
+        lcd.cursor_pos = (1, 2)
+        lcd.write_string('#  ')
+        lcd.cursor_pos = (1, 16)
+        lcd.write_string('  #')
+        lcd.cursor_pos = (0, 5)
+        lcd.write_string('#   ')
+        lcd.cursor_pos = (0, 12)
+        lcd.write_string('   #')
+
+        sleep(0.01)
+
+        lcd.cursor_pos = (2, 5)
+        lcd.write_string('  ')
+        lcd.cursor_pos = (2, 14)
+        lcd.write_string('  ')
+        lcd.cursor_pos = (1, 2)
+        lcd.write_string('   ')
+        lcd.cursor_pos = (1, 16)
+        lcd.write_string('   ')
+        lcd.cursor_pos = (0, 5)
+        lcd.write_string('    ')
+        lcd.cursor_pos = (0, 12)
+        lcd.write_string('    ')
+
+        sleep(0.01)
+
+
+    sleep(1)
+    lcd.clear()
